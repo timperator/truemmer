710fe3d7c67111340d9f84068ee0d7a860d86f81
Tim H <tim@heidelba.ch>
2021-12-03 14:53:45 +0100

    Fix asset path

--- src/app/truemmerdialog/truemmerdialog.component.ts
+++ src/app/truemmerdialog/truemmerdialog.component.ts
@@ -26,23 +26,23 @@ function playZwirnie(truemmerfaktor: number) {
   let audio = new Audio();
 
   if (truemmerfaktor > 20) {
-    audio.src = "../../../assets/audio/lachen.mp3";
+    audio.src = "assets/audio/lachen.mp3";
   } else if (truemmerfaktor > 15) {
-    audio.src = "../../../assets/audio/sinnlos.mp3";
+    audio.src = "assets/audio/sinnlos.mp3";
   } else if (truemmerfaktor > 10) {
-    audio.src = "../../../assets/audio/uff.mp3";
+    audio.src = "assets/audio/uff.mp3";
   } else if (truemmerfaktor > 7) {
-    audio.src = "../../../assets/audio/aehmja.mp3";
+    audio.src = "assets/audio/aehmja.mp3";
   } else if (truemmerfaktor > 5) {
-    audio.src = "../../../assets/audio/stoehnen.mp3";
+    audio.src = "assets/audio/stoehnen.mp3";
   } else if (truemmerfaktor > 3) {
-    audio.src = "../../../assets/audio/nichtschlecht.mp3";
+    audio.src = "assets/audio/nichtschlecht.mp3";
   } else if (truemmerfaktor > 2) {
-    audio.src = "../../../assets/audio/wunderbar.mp3";
+    audio.src = "assets/audio/wunderbar.mp3";
   } else if (truemmerfaktor > 1) {
-    audio.src = "../../../assets/audio/traeumchen.mp3";
+    audio.src = "assets/audio/traeumchen.mp3";
   } else {
-    audio.src = "../../../assets/audio/richtiggeil.mp3";
+    audio.src = "assets/audio/richtiggeil.mp3";
   }
 
   audio.load();
