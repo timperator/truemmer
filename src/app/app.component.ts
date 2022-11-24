204a17224d6b0301aa23e0e2ef94e308c7b6f1e8
Tim H <tim@heidelba.ch>
2022-11-24 11:13:35 +0100

    Update dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatLegacyDialog as MatDialog, MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA} from '@angular/material/legacy-dialog';
 import { TruemmerdialogComponent } from './truemmerdialog/truemmerdialog.component';
 
 interface Raid {
