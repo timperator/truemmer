b69a26524f1ebebd26b4c65a94c04b3ee2366dad
Tim H <tim@heidelba.ch>
2022-12-29 10:50:20 +0100

    Update raid complexities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output } from '@angular/core';
-import {MatLegacyDialog as MatDialog, MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA} from '@angular/material/legacy-dialog';
+import {MatDialog as MatDialog, MatDialogRef as MatDialogRef, MAT_DIALOG_DATA as MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { TruemmerdialogComponent } from './truemmerdialog/truemmerdialog.component';
 
 interface Raid {
@@ -27,17 +27,17 @@ export class AppComponent {
   motivation = 1;
 
   raids: Raid[] = [
-    { viewValue: 'The Nature of Progress', complexity: 9 },
+    { viewValue: 'The Nature of Progress', complexity: 8 },
     { viewValue: 'Eternity Vault', complexity: 1 },
     { viewValue: 'Gods from the Machine', complexity: 10 },
-    { viewValue: 'Explosive Conflict', complexity: 6 },
+    { viewValue: 'Explosive Conflict', complexity: 7 },
     { viewValue: "Karagga's Palace", complexity: 1 },
-    { viewValue: 'Scum and Villainy', complexity: 5 },
-    { viewValue: 'Temple of Sacrifice', complexity: 7 },
+    { viewValue: 'Scum and Villainy', complexity: 4 },
+    { viewValue: 'Temple of Sacrifice', complexity: 6 },
     { viewValue: 'Terror from Beyond', complexity: 4 },
-    { viewValue: 'The R-4 Anomaly', complexity: 10 },
-    { viewValue: 'The Dread Fortress', complexity: 7 },
-    { viewValue: 'The Dread Palace', complexity: 8 },
+    { viewValue: 'The R-4 Anomaly', complexity: 8 },
+    { viewValue: 'The Dread Fortress', complexity: 5 },
+    { viewValue: 'The Dread Palace', complexity: 6 },
     { viewValue: 'The Ravagers', complexity: 6 },
   ];
 
